ea0bf84f4f4000912f2ee7648cb876db79e1f3c4
CJW-MAPU <c.j.biz.woong@gmail.com>
2021-06-15 05:52:52 +0900

    #create project

---
+++ server/src/test/java/kr/ac/pcu/cyber/studentservice/StudentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.pcu.cyber.studentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
